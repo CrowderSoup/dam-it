4393901b5021b71817559b47522592a3d54a4f1c
CrowderSoup <aaron.crowder@gmail.com>
2026-09-14 16:24:20 -0600

    Add a "journal" input action (keyboard J, gamepad Y)
    Opens/closes the new objective journal - see the following commits.

--- scripts/autoload/input_setup.gd
+++ scripts/autoload/input_setup.gd
@@ -27,6 +27,7 @@ func _ready() -> void:
 	_bind("interact", KEY_E, KEY_SPACE)
 	_bind("restart", KEY_R)
 	_bind("menu", KEY_ESCAPE)
+	_bind("journal", KEY_J)
 
 	_add_joy_button("move_up", JOY_BUTTON_DPAD_UP)
 	_add_joy_button("move_down", JOY_BUTTON_DPAD_DOWN)
@@ -35,6 +36,7 @@ func _ready() -> void:
 	_add_joy_button("interact", JOY_BUTTON_A)
 	_add_joy_button("menu", JOY_BUTTON_START)
 	_add_joy_button("menu", JOY_BUTTON_BACK)
+	_add_joy_button("journal", JOY_BUTTON_Y)
 
 	_add_joy_axis("move_up", JOY_AXIS_LEFT_Y, -1.0)
 	_add_joy_axis("move_down", JOY_AXIS_LEFT_Y, 1.0)
